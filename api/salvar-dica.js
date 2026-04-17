64e47b1a807e7643fa39c9fe9bad288e48e16335
NtDuarte <northon.luizz@gmail.com>
2026-04-16 22:42:49 -0300

    Update salvar-dica.js

--- api/salvar-dica.js
+++ api/salvar-dica.js
@@ -35,7 +35,7 @@ Data: ${agora.toLocaleString('pt-BR')}
 `;
 
         await put(nomeArquivo, conteudo, {
-            access: 'private',
+            access: 'public',
             addRandomSuffix: false,
             contentType: 'text/plain; charset=utf-8'
         });
